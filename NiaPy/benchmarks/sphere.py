b23b5be25e1484654c3e0ec3b55926a4fa693020
firefly-cpp <pingvin1@gmail.com>
2018-02-07 09:37:21 +0100

    an example of benchmark function

---
+++ NiaPy/benchmarks/sphere.py
@@ -0,0 +1,13 @@
+"""Implementation of Sphere function"""
+
+
+class Sphere:
+    def __init__(self, D, sol):
+        self.D = D
+        self.sol = sol
+
+    def evaluate(self):
+        val = 0.0
+        for i in range(self.D):
+            val = val + self.sol[i] * self.sol[i]
+        return val
