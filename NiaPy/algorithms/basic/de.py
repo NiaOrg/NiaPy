dfab1a634ab8b9ecc9ab3e26c802bdd34d6b2c50
firefly-cpp <pingvin1@gmail.com>
2018-02-08 15:25:05 +0100

    added reference for DE

--- NiaPy/algorithms/basic/de.py
+++ NiaPy/algorithms/basic/de.py
@@ -6,6 +6,8 @@ Authors : Uros Mlakar
 
 License: MIT
 
+Reference paper: Storn, Rainer, and Kenneth Price. "Differential evolution - a simple and efficient heuristic for global optimization over continuous spaces." Journal of global optimization 11.4 (1997): 341-359.
+
 TODO
 """
 
