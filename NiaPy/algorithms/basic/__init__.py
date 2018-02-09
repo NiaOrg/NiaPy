f9c33ec320a4acac1f8991d123fd97a438b95995
GregaVrbancic <grega.vrbancic@gmail.com>
2018-02-09 12:38:58 +0100

    add fpa example. update README.md with example.

--- NiaPy/algorithms/basic/__init__.py
+++ NiaPy/algorithms/basic/__init__.py
@@ -3,9 +3,11 @@
 from NiaPy.algorithms.basic.ba import BatAlgorithm
 from NiaPy.algorithms.basic.fa import FireflyAlgorithm
 from NiaPy.algorithms.basic.de import DifferentialEvolutionAlgorithm
+from NiaPy.algorithms.basic.fpa import FlowerPollinationAlgorithm
 
 __all__ = [
     'BatAlgorithm',
     'FireflyAlgorithm',
-    'DifferentialEvolutionAlgorithm'
+    'DifferentialEvolutionAlgorithm',
+    'FlowerPollinationAlgorithm'
 ]
