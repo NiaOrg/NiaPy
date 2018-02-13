25f3b4f54f133caca6c65d4bebc955974715a794
GregaVrbancic <grega.vrbancic@gmail.com>
2018-02-13 08:27:02 +0100

    add support for custom benchmark in runner.

--- examples/run.py
+++ examples/run.py
@@ -15,7 +15,22 @@ logger.setLevel('INFO')
 # For reproducive results
 random.seed(1234)
 
+
+class MyBenchmark(object):
+    def __init__(self):
+        self.Lower = -11
+        self.Upper = 11
+
+    def function(self):
+        def evaluate(D, sol):
+            val = 0.0
+            for i in range(D):
+                val = val + sol[i] * sol[i]
+            return val
+        return evaluate
+
+
 algorithms = ['BatAlgorithm', 'DifferentialEvolutionAlgorithm']
-benchmarks = ['griewank', 'ackley', 'sphere']
+benchmarks = ['griewank', 'ackley', 'sphere', MyBenchmark()]
 
 NiaPy.Runner(10, 40, 1000, 10, algorithms, benchmarks).run()
