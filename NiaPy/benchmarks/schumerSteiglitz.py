cbf136290b3d3cc51c6ec6d4810fb40a99aecba2
lucijabrezocnik <36370699+lucijabrezocnik@users.noreply.github.com>
2018-02-18 14:33:06 +0100

    Schumer Steiglitz Function
    new function

---
+++ NiaPy/benchmarks/schumerSteiglitz.py
@@ -0,0 +1,51 @@
+# encoding=utf8
+# pylint: disable=anomalous-backslash-in-string
+"""Implementation of Schumer Steiglitz function.
+
+Date: 2018
+
+Author: Lucija Brezočnik
+
+License: MIT
+
+Function: Schumer Steiglitz function
+
+Input domain:
+    The function can be defined on any input domain but it is usually
+    evaluated on the hypercube x_i ∈ [-100, 100], for all i = 1, 2,..., D.
+
+LaTeX formats:
+    Inline: $f(\mathbf{x}) = \sum_{i=1}^D x_i^4$
+    Equation:  \begin{equation} f(\mathbf{x}) = \sum_{i=1}^D x_i^4 \end{equation}
+    Domain: $-100 \leq x_i \leq 100$
+
+Reference paper:
+    Jamil, M., and Yang, X. S. (2013).
+    A literature survey of benchmark functions for global optimisation problems.
+    International Journal of Mathematical Modelling and Numerical Optimisation,
+    4(2), 150-194.
+"""
+
+import math
+
+__all__ = ['SchumerSteiglitz']
+
+
+class SchumerSteiglitz(object):
+
+    def __init__(self, Lower=-100, Upper=100):
+        self.Lower = Lower
+        self.Upper = Upper
+
+    @classmethod
+    def function(cls):
+        def evaluate(D, sol):
+
+            val = 0.0
+
+            for i in range(D):
+                val += math.pow(sol[i], 4)
+
+            return val
+
+        return evaluate
