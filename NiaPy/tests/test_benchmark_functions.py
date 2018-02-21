2bd1f8a824f36bc1b9124704ee28c8c31aa01f3b
lucijabrezocnik <36370699+lucijabrezocnik@users.noreply.github.com>
2018-02-21 15:30:00 +0100

    tests added

--- NiaPy/tests/test_benchmark_functions.py
+++ NiaPy/tests/test_benchmark_functions.py
@@ -5,11 +5,16 @@ from NiaPy.benchmarks.utility import Utility
 
 class TestBenchmarkFunctions(TestCase):
 
+    # pylint: disable=too-many-instance-attributes,too-many-public-methods
     def setUp(self):
         self.D = 5
         self.array = [0, 0, 0, 0, 0]
         self.array2 = [1, 1, 1, 1, 1]
-        self.array3 = [420.9687, 420.9687, 420.9687, 420.9687, 420.9687]
+        self.array3 = [420.968746, 420.968746, 420.968746, 420.968746, 420.968746]
+        self.array4 = [-2.903534, -2.903534, -2.903534, -2.903534, -2.903534]
+        self.array5 = [0.5, 0.5, 0.5, 0.5, 0.5]
+        self.array6 = [-1, -1, -1, -1, -1]
+        self.array7 = [2, 2, 2, 2, 2]
 
     def test_rastrigin(self):
         rastrigin = Utility().get_benchmark('rastrigin')
@@ -39,7 +44,7 @@ class TestBenchmarkFunctions(TestCase):
     def test_schwefel(self):
         schwefel = Utility().get_benchmark('schwefel')
         fun = schwefel.function()
-        self.assertAlmostEqual(round(fun(self.D, self.array3)), 0.0)
+        self.assertEqual(round(fun(self.D, self.array3)), 0.0)
 
     def test_schwefel221(self):
         schwefel221 = Utility().get_benchmark('schwefel221')
@@ -55,3 +60,58 @@ class TestBenchmarkFunctions(TestCase):
         whitley = Utility().get_benchmark('whitley')
         fun = whitley.function()
         self.assertEqual(fun(self.D, self.array2), 0.0)
+
+    def test_styblinskiTang(self):
+        styblinskiTang = Utility().get_benchmark('styblinskiTang')
+        fun = styblinskiTang.function()
+        self.assertTrue(fun(self.D, self.array4) < -78.332)
+
+    def test_sumSquares(self):
+        sumSquares = Utility().get_benchmark('sumSquares')
+        fun = sumSquares.function()
+        self.assertEqual(fun(self.D, self.array), 0.0)
+
+    def test_stepint(self):
+        stepint = Utility().get_benchmark('stepint')
+        fun = stepint.function()
+        self.assertEqual(fun(self.D, self.array), 25.0)
+
+    def test_step(self):
+        step = Utility().get_benchmark('step')
+        fun = step.function()
+        self.assertEqual(fun(self.D, self.array), 0.0)
+
+    def test_step2(self):
+        step2 = Utility().get_benchmark('step2')
+        fun = step2.function()
+        self.assertEqual(fun(self.D, self.array5), 5.0)
+
+    def test_step3(self):
+        step3 = Utility().get_benchmark('step3')
+        fun = step3.function()
+        self.assertEqual(fun(self.D, self.array), 0.0)
+
+    def test_schumerSteiglitz(self):
+        schumerSteiglitz = Utility().get_benchmark('schumerSteiglitz')
+        fun = schumerSteiglitz.function()
+        self.assertEqual(fun(self.D, self.array), 0.0)
+
+    def test_salomon(self):
+        salomon = Utility().get_benchmark('salomon')
+        fun = salomon.function()
+        self.assertEqual(fun(self.D, self.array), 0.0)
+
+    def test_quintic(self):
+        quintic = Utility().get_benchmark('quintic')
+        fun = quintic.function()
+        self.assertEqual(fun(self.D, self.array6), 0.0)
+
+    def test_quintic2(self):
+        quintic = Utility().get_benchmark('quintic')
+        fun = quintic.function()
+        self.assertEqual(fun(self.D, self.array7), 0.0)
+
+    def test_pinter(self):
+        pinter = Utility().get_benchmark('pinter')
+        fun = pinter.function()
+        self.assertEqual(fun(self.D, self.array), 0.0)
