53779d695d6b891661ad5f542cd8f29d45de38a7
firefly-cpp <pingvin1@gmail.com>
2018-02-24 22:27:43 +0100

    more tests for FA & FPA

--- NiaPy/tests/test_fa.py
+++ NiaPy/tests/test_fa.py
@@ -6,8 +6,8 @@ from NiaPy.algorithms.basic import FireflyAlgorithm
 class MyBenchmark(object):
 
     def __init__(self):
-        self.Lower = -11
-        self.Upper = 11
+        self.Lower = -5.12
+        self.Upper = 5.12
 
     @classmethod
     def function(cls):
@@ -20,9 +20,16 @@ class MyBenchmark(object):
 
 
 class FATestCase(TestCase):
+
     def setUp(self):
         self.fa = FireflyAlgorithm(
             10, 20, 10000, 0.5, 0.2, 1.0, MyBenchmark())
 
+        self.fa_griewank = FireflyAlgorithm(
+            10, 20, 10000, 0.5, 0.2, 1.0, 'griewank')
+
     def test_works_fine(self):
         self.assertTrue(self.fa.run())
+
+    def test_griewank_works_fine(self):
+        self.assertTrue(self.fa_griewank.run())
--- NiaPy/tests/test_fpa.py
+++ NiaPy/tests/test_fpa.py
@@ -6,8 +6,8 @@ from NiaPy.algorithms.basic import FlowerPollinationAlgorithm
 class MyBenchmark(object):
 
     def __init__(self):
-        self.Lower = -11
-        self.Upper = 11
+        self.Lower = -5.12
+        self.Upper = 5.12
 
     @classmethod
     def function(cls):
@@ -20,9 +20,16 @@ class MyBenchmark(object):
 
 
 class FPATestCase(TestCase):
+
     def setUp(self):
         self.fpa_custom = FlowerPollinationAlgorithm(
             10, 20, 10000, 0.5, MyBenchmark())
 
+        self.fpa_griewank = FlowerPollinationAlgorithm(
+            10, 20, 10000, 0.5, 'griewank')
+
     def test_custom_works_fine(self):
         self.assertTrue(self.fpa_custom.run())
+
+    def test_griewank_works_fine(self):
+        self.assertTrue(self.fpa_griewank.run())
