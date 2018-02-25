e9c4c608bb4fc7bea49524da84004a5d16d7a929
Grega Vrbančič <grega.vrbancic@gmail.com>
2018-02-25 12:58:39 +0100

    fix docs

--- examples/basic_customized_example.py
+++ examples/basic_customized_example.py
@@ -5,19 +5,19 @@ sys.path.append('../')
 # End of fix
 
 from NiaPy.algorithms.basic import GreyWolfOptimizer
-from NiaPy.benchmarks import HappyCat
+from NiaPy.benchmarks import Pinter
 
-# initialize Happy Cat benchamrk with custom bound
-happyCatCustom = HappyCat(-50, 50)
+# initialize Pinter benchamrk with custom bound
+pinterCustom = Pinter(-5, 5)
 
-# we will run 10 repetitions of Grey Wolf Optimizer against Happy Cat benchmark function
+# we will run 10 repetitions of Grey Wolf Optimizer against Pinter benchmark function
 for i in range(10):
     # first parameter takes dimension of problem
     # second parameter is population size
     # third parameter takes the number of function evaluations
     # fourth parameter is benchmark function 
-    algorithm = GreyWolfOptimizer(10, 20 , 10000, happyCatCustom)
-        
+    algorithm = GreyWolfOptimizer(10, 20 , 10000, pinterCustom)
+
     # running algorithm returns best found minimum
     best = algorithm.run()
 
