8cd34624a367948a7e28c52afa1eadea98460df6
GregaVrbancic <grega.vrbancic@gmail.com>
2018-02-27 10:46:51 +0100

    fix

--- NiaPy/algorithms/basic/cs.py
+++ NiaPy/algorithms/basic/cs.py
@@ -75,7 +75,7 @@ class CuckooSearchAlgorithm(object):
             benchmark {object} -- benchmark implementation object
 
         """
-        print(benchmark)
+
         self.benchmark = Utility().get_benchmark(benchmark)
         self.Np = Np  # population size
         self.D = D  # dimension
