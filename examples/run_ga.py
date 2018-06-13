a107b0c0ecb6916035b831876c5594de23033599
rhododendrom <dule.fister@gmail.com>
2018-06-13 12:03:14 +0200

    Update run_ga.py

--- examples/run_ga.py
+++ examples/run_ga.py
@@ -5,11 +5,33 @@ sys.path.append('../')
 # End of fix
 
 import random
+import logging
 from NiaPy.algorithms.basic import GeneticAlgorithm
+
+logging.basicConfig()
+logger = logging.getLogger('examples')
+logger.setLevel('INFO')
+
+# For reproducive results
+random.seed(1234)
+
+
+class MyBenchmark(object):
+    def __init__(self):
+        self.Lower = -11
+        self.Upper = 11
+
+    def function(self):
+        def evaluate(D, sol):
+            val = 0.0
+            for i in range(D):
+                val = val + sol[i] * sol[i]
+            return val
+        return evaluate
 
 
 for i in range(10):
-    Algorithm = GeneticAlgorithm(10, 40, 10000, 4, 0.05, 0.4, 'sphere')
+    Algorithm = GeneticAlgorithm(10, 40, 10000, 4, 0.05, 0.4, MyBenchmark())
     Best = Algorithm.run()
 
-    print(Best)
+    logger.info(Best)
