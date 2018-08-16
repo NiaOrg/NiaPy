8d29ca495d977611b0fa60f2037c035676adf9ee
Klemen <roxor1992@gmail.com>
2018-08-16 14:52:07 +0200

    Fix for simulated annealing algorithm

--- NiaPy/algorithms/other/sa.py
+++ NiaPy/algorithms/other/sa.py
@@ -26,33 +26,29 @@ class SimulatedAnnealing(Algorithm):
 	**Reference URL:**
 	**Reference paper:**
 	"""
-	def __init__(self, **kwargs):
-		r"""Init Simulated Annealing Algorithm.
+	def __init__(self, **kwargs): Algorithm.__init__(self, name='SimulatedAnnealing', sName='BBFA', **kwargs)
 
-		**See**:
-		Algorithm.__init__(self, **kwargs)
-		"""
-		Algorithm.__init__(self, name='SimulatedAnnealing', sName='BBFA', **kwargs)
-
-	def setParameters(self, delta=0.5, T=20, deltaT=0.8, coolingMethod=coolDelta, **ukwargs):
+	def setParameters(self, delta=0.5, T=20, deltaT=0.8, coolingMethod=coolDelta, epsilon=1e-23, **ukwargs):
 		r"""Set the algorithm parameters/arguments.
 
-		**See**:
-		SimulatedAnnealing.__setparams(self, n=10, c_a=1.5, c_r=0.5, **ukwargs)
+		Arguments:
+		delta {real} --
+		T {real} --
+		deltaT {real} --
+		coolingMethod {function} --
+		epsilon {real}
 		"""
-		self.delta, self.T, self.curT, self.deltaT, self.cool = delta, T, T, deltaT, coolingMethod
+		self.delta, self.T, self.deltaT, self.cool, self.epsilon = delta, T, deltaT, coolingMethod, epsilon
 		if ukwargs: logger.info('Unused arguments: %s' % (ukwargs))
 
 	def runTask(self, task):
 		x = task.Lower + task.bRange * self.rand(task.D)  # Random solution
-		curT = self.T
-		xfit = task.eval(x)
-		while not task.stopCond() or (curT < 0):
-			c = x - self.delta / 2 + self.rand(task.D) * self.delta
-			c = task.repair(c)
+		curT, xfit = self.T, task.eval(x)
+		while not task.stopCond():
+			if (curT <= self.epsilon): curT = self.T
+			c = task.repair(x - self.delta / 2 + self.rand(task.D) * self.delta)
 			cfit = task.eval(c)
-			deltaFit = cfit - xfit
-			rand = self.rand()
+			deltaFit, rand = cfit - xfit, self.rand()
 			if deltaFit < 0 or rand < exp(deltaFit / curT): x, xfit = c, cfit
 			curT = self.cool(curT, self.T, self.deltaT, nFES=task.nFES)
 		return x, xfit
