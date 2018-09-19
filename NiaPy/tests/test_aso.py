e81c640da9e76e666c78a41eb2ecdcfc6683a422
Klemen <roxor1992@gmail.com>
2018-09-19 16:50:12 +0200

    Update test_aso.py

--- NiaPy/tests/test_aso.py
+++ NiaPy/tests/test_aso.py
@@ -1,5 +1,6 @@
 # encoding=utf8
 # pylint: disable=mixed-indentation, multiple-statements, old-style-class, line-too-long
+from unittest import TestCase
 from NiaPy.tests.test_algorithm import AlgorithmTestCase, MyBenchmark
 from NiaPy.benchmarks.griewank import Griewank
 from NiaPy.algorithms.other import AnarchicSocietyOptimization
