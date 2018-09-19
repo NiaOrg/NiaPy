522fc3635eb107cfb3023484fe168aa5eb048374
rhododendrom <dule.fister@gmail.com>
2018-09-19 17:02:21 +0200

    Update jde.py

--- NiaPy/algorithms/modified/jde.py
+++ NiaPy/algorithms/modified/jde.py
@@ -83,7 +83,7 @@ class DynNPSelfAdaptiveDifferentialEvolutionAlgorithm(SelfAdaptiveDifferentialEv
 	**Reference URL:** https://link.springer.com/article/10.1007/s10489-007-0091-x
 
 	**Reference paper:** Brest, Janez, and Mirjam Sepesy Maučec.
-	"Population size reduction for the differential evolution algorithm."
+	Population size reduction for the differential evolution algorithm.
 	Applied Intelligence 29.3 (2008): 228-247.
 	"""
 	def __init__(self, **kwargs): SelfAdaptiveDifferentialEvolutionAlgorithm.__init__(self, name='SelfAdaptiveDifferentialEvolutionAlgorithm', sName='jDE', **kwargs)
