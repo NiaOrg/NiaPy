3e9378980af8d653728f15129ae758496b41ef95
firefly-cpp <pingvin1@gmail.com>
2018-11-22 12:41:05 +0100

    proposal of basic statistics

--- NiaPy/algorithms/__init__.py
+++ NiaPy/algorithms/__init__.py
@@ -4,5 +4,6 @@ from NiaPy.algorithms import basic
 from NiaPy.algorithms import modified
 from NiaPy.algorithms import other
 from NiaPy.algorithms.algorithm import Algorithm, Individual
+from NiaPy.algorithms.statistics import BasicStatistics
 
-__all__ = ['basic', 'modified', 'other', 'Algorithm', 'Individual']
+__all__ = ['basic', 'modified', 'other', 'Algorithm', 'Individual', 'BasicStatistics']
