14b58939d4b82478f65352b4f4dbf7f7dd9107a2
firefly-cpp <pingvin1@gmail.com>
2018-11-25 20:15:20 +0100

    new tests

--- NiaPy/tests/test_iter_gen_counters.py
+++ NiaPy/tests/test_iter_gen_counters.py
@@ -11,18 +11,82 @@ from unittest import TestCase
 
 class DETestCase(TestCase):
     r"""Test cases for evaluating different stopping conditions.
-	
-	**Date:** November 2018
-	
-	**Author:** Iztok 
-	
-	**Author:** This is a very important test!
-	"""
-    
-    #add scenario description
+
+        **Date:** November 2018
+
+        **Author:** Iztok
+
+        **Author:** This is a very important test!
+        """
+
+    # add scenario description
     def test_DE_evals_fine(self):
-        task = Task(D=10, nFES=1000, optType=OptimizationType.MINIMIZATION, benchmark=Sphere())
-        algo = DifferentialEvolution(task=task, NP=40, CR=0.9, F=0.5)   
+        task = Task(
+            D=10,
+            nFES=1000,
+            optType=OptimizationType.MINIMIZATION,
+            benchmark=Sphere())
+        algo = DifferentialEvolution(task=task, NP=40, CR=0.9, F=0.5)
         best = algo.run()
         evals = algo.task.evals()
         self.assertEqual(evals, 1000)
+
+     # add scenario description
+    def test_DE_iters_fine(self):
+        task = Task(
+            D=10,
+            nGEN=1000,
+            optType=OptimizationType.MINIMIZATION,
+            benchmark=Sphere())
+        algo = DifferentialEvolution(task=task, NP=40, CR=0.9, F=0.5)
+        best = algo.run()
+        iters = algo.task.iters()
+        self.assertEqual(iters, 1000)
+
+
+class BATestCase(TestCase):
+    r"""Test cases for evaluating different stopping conditions.
+
+        **Date:** November 2018
+
+        **Author:** Iztok
+
+        **Author:** This is a very important test!
+        """
+
+    # add scenario description
+    def test_BA_evals_fine(self):
+        task = Task(
+            D=10,
+            nFES=1000,
+            optType=OptimizationType.MINIMIZATION,
+            benchmark=Sphere())
+        algo = BatAlgorithm(task=task, NP=25)
+        best = algo.run()
+        evals = algo.task.evals()
+        self.assertEqual(evals, 1000)
+
+    # add scenario description 
+    def test_BA_iters_fine(self):
+        task = Task(
+            D=10,
+            nGEN=1000,
+            optType=OptimizationType.MINIMIZATION,
+            benchmark=Sphere())
+        algo = BatAlgorithm(task=task, NP=25)
+        best = algo.run()
+        iters = algo.task.iters()
+        self.assertEqual(iters, 1000)
+        
+
+    # 1000 BA iterations spends 10010 FES (10 + 10 * 1000)
+    def test_BA_iters_to_fes(self):
+        task = Task(
+            D=10,
+            nGEN=1000,
+            optType=OptimizationType.MINIMIZATION,
+            benchmark=Sphere())
+        algo = BatAlgorithm(task=task, NP=10)
+        best = algo.run()
+        evals = algo.task.evals()
+        self.assertEqual(evals, 10010)        
