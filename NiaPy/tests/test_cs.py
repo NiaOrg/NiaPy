c16ad16110ec981b0ba0b315c2caf6e13e29f33c
Klemen Berkovic <roxor1992@gmail.com>
2019-02-01 14:49:00 +0100

    Added CS algorithm and test for this algorithm

---
+++ NiaPy/tests/test_cs.py
@@ -0,0 +1,17 @@
+# encoding=utf8
+# pylint: disable=mixed-indentation, disable=too-many-function-args
+from NiaPy.tests.test_algorithm import AlgorithmTestCase, MyBenchmark
+from NiaPy.algorithms.basic import CuckooSearch
+
+class CSTestCase(AlgorithmTestCase):
+	def test_custom_works_fine(self):
+		cs_custom = CuckooSearch(D=self.D, NP=20, nFES=self.nFES, nGEN=self.nGEN, benchmark=MyBenchmark(), seed=self.seed)
+		cs_customc = CuckooSearch(D=self.D, NP=20, nFES=self.nFES, nGEN=self.nGEN, benchmark=MyBenchmark(), seed=self.seed)
+		AlgorithmTestCase.algorithm_run_test(self, cs_custom, cs_customc)
+
+	def test_griewank_works_fine(self):
+		cs_griewank = CuckooSearch(NP=10, D=self.D, nFES=self.nFES, nGEN=self.nGEN, benchmark='griewank', seed=self.seed)
+		cs_griewankc = CuckooSearch(NP=10, D=self.D, nFES=self.nFES, nGEN=self.nGEN, benchmark='griewank', seed=self.seed)
+		AlgorithmTestCase.algorithm_run_test(self, cs_griewank, cs_griewankc)
+
+# vim: tabstop=3 noexpandtab shiftwidth=3 softtabstop=3
