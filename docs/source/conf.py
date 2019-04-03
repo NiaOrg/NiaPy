f71546316288287e8cec60b5dfc9ec808ebd6853
Klemen <roxor1992@gmail.com>
2019-04-03 23:47:30 +0200

    Update conf.py
    Added fix for napolen

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
