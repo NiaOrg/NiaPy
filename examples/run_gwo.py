51b83b84abd275c56fe1502cf4df5af6574eb518
firefly-cpp <pingvin1@gmail.com>
2019-04-11 00:41:22 +0200

    Simplified examples - part 2 (#190)
    Add simple examples for:
    
    * CS
    * FWA
    * GSA
    * GSO
    * GWO

--- examples/run_gwo.py
+++ examples/run_gwo.py
@@ -1,36 +1,17 @@
+# encoding=utf8
 # This is temporary fix to import module from parent folder
 # It will be removed when package is published on PyPI
 import sys
 sys.path.append('../')
 # End of fix
 
-import random
-import logging
 from NiaPy.algorithms.basic import GreyWolfOptimizer
+from NiaPy.util import StoppingTask, OptimizationType
+from NiaPy.benchmarks import Sphere
 
-logging.basicConfig()
-logger = logging.getLogger('examples')
-logger.setLevel('INFO')
-
-# For reproducive results
-random.seed(1234)
-
-
-class MyBenchmark(object):
-    def __init__(self):
-        self.Lower = -11
-        self.Upper = 11
-
-    def function(self):
-        def evaluate(D, sol):
-            val = 0.0
-            for i in range(D):
-                val = val + sol[i] * sol[i]
-            return val
-        return evaluate
-
-
-for i in range(10):
-    Algorithm = GreyWolfOptimizer(D=10, NP=20, nFES=10000, benchmark=MyBenchmark())
-    Best = Algorithm.run()
-    logger.info(Best)
+# we will run Grey Wolf Optimizer for 5 independent runs
+for i in range(5):
+    task = StoppingTask(D=10, nFES=10000, optType=OptimizationType.MINIMIZATION, benchmark=Sphere())
+    algo = GreyWolfOptimizer(NP=40)
+    best = algo.run(task=task)
+    print(best)
