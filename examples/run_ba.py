943ad15cea58953e805640f0d17de3a5fa78bc58
firefly-cpp <pingvin1@gmail.com>
2019-04-14 12:17:06 +0200

    more modifie examples

--- examples/run_ba.py
+++ examples/run_ba.py
@@ -11,7 +11,7 @@ from NiaPy.benchmarks import Sphere
 
 # we will run Bat Algorithm for 5 independent runs
 for i in range(5):
-    task = StoppingTask(D=10, nFES=10000, optType=OptimizationType.MINIMIZATION, benchmark=Sphere())
-    algo = BatAlgorithm(NP=40, A=0.7, r=0.7, Qmin=0.0, Qmax=2.0)
+    task = StoppingTask(D=10, nGEN=1000, optType=OptimizationType.MINIMIZATION, benchmark=Sphere())
+    algo = BatAlgorithm(NP=40, A=0.5, r=0.5, Qmin=0.0, Qmax=2.0)
     best = algo.run(task=task)
-    print best
+    print (best)
