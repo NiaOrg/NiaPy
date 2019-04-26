26305498af3bec59f8012f9ae909e6130cc3aba3
firefly-cpp <pingvin1@gmail.com>
2019-04-26 22:31:52 +0200

    minor fix in examples

--- examples/log_results.py
+++ examples/log_results.py
@@ -5,13 +5,13 @@ import sys
 sys.path.append('../')
 # End of fix
 
-from NiaPy.task.task import TaskConvSave, OptimizationType
+from NiaPy.task.task import StoppingTask, OptimizationType
 from NiaPy.algorithms.basic import DifferentialEvolution 
 from NiaPy.benchmarks import Sphere
 
 # Storing improvements during the evolutionary cycle
 for i in range(1):
-	task = TaskConvSave(D=10, nFES=10000, optType=OptimizationType.MINIMIZATION, benchmark=Sphere())
+	task = StoppingTask(D=10, nFES=10000, optType=OptimizationType.MINIMIZATION, benchmark=Sphere())
 	algo = DifferentialEvolution(NP=40, CR=0.9, F=0.5)
 	best = algo.run(task)
 	evals, x_f = task.return_conv()
