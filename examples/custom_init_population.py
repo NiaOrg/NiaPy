0588f228691a6ae32450319076abb1b1d08d8326
firefly-cpp <pingvin1@gmail.com>
2019-04-28 19:47:39 +0200

    fixed example

--- examples/custom_init_population.py
+++ examples/custom_init_population.py
@@ -10,15 +10,15 @@ from NiaPy.task.task import StoppingTask, OptimizationType
 from NiaPy.benchmarks import Sphere
 from numpy import random as rand, apply_along_axis
 
-def customInit(task, NP, rnd=rand):
-	pop = task.Lower + rnd.rand(NP, task.D) * 2
-	fpop = apply_along_axis(task.eval, 1, pop)
-	return pop, fpop
+def MyInit(task, NP, rnd=rand, **kwargs):
+    pop = 0.2 + rnd.rand(NP, task.D) * task.bRange
+    fpop = apply_along_axis(task.eval, 1, pop)
+    return pop, fpop
 
 
 # we will run Particle Swarm Algorithm with custom Init function for 5 independent runs
 for i in range(5):
-	task = StoppingTask(D=10, nFES=1000, optType=OptimizationType.MINIMIZATION, InitPopFunc=customInit, benchmark=Sphere())
-	algo = ParticleSwarmAlgorithm(NP=10, C1=2.0, C2=2.0, w=0.7, vMin=-4, vMax=4)
+	task = StoppingTask(D=10, nFES=1000, optType=OptimizationType.MINIMIZATION, benchmark=Sphere())
+	algo = ParticleSwarmAlgorithm(NP=10, C1=2.0, C2=2.0, w=0.7, vMin=-4, vMax=4, InitPopFunc=MyInit)
 	best = algo.run(task=task)
 	print(best)
