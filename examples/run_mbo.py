fa18805160b62dccf9855c5d1f61f4962592cf9c
Jan Banko <janbanko996@gmail.com>
2019-04-29 19:40:50 +0200

    Contributor table fix, run_mbo example fix.

--- examples/run_mbo.py
+++ examples/run_mbo.py
@@ -1,18 +1,18 @@
 # encoding=utf8
 # This is temporary fix to import module from parent folder
 # It will be removed when package is published on PyPI
-from NiaPy.benchmarks import Sphere
-from NiaPy.util import StoppingTask, OptimizationType
 from NiaPy.algorithms.basic import MonarchButterflyOptimization
+from NiaPy.task.task import StoppingTask, OptimizationType
+from NiaPy.benchmarks import Sphere
 import random
 import sys
 sys.path.append('../')
 # End of fix
 
 
-# we will run Forest Optimization Algorithm for 5 independent runs
+# we will run Monarch Butterfly Optimization algorithm for 5 independent runs
 for i in range(5):
     task = StoppingTask(D=10, nFES=100000, optType=OptimizationType.MINIMIZATION, benchmark=Sphere())
-    algo = MonarchButterflyOptimization(NP=50)
+    algo = MonarchButterflyOptimization(NP=20, PAR=5.0 / 12.0, PER=1.2)
     best = algo.run(task=task)
-    print(best)
+    print('%s -> %s' % (best[0], best[1]))
