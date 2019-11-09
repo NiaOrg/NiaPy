b642ed25e7aaa5984a24030ec308774728c5382b
Klemen@Haxor <roxor1992@gmail.com>
2019-11-09 14:42:59 +0100

    Added fixes for CS algorithm for the new interface

--- NiaPy/tests/test_cs.py
+++ NiaPy/tests/test_cs.py
@@ -4,14 +4,18 @@ from NiaPy.tests.test_algorithm import AlgorithmTestCase, MyBenchmark
 from NiaPy.algorithms.basic import CuckooSearch
 
 class CSTestCase(AlgorithmTestCase):
+	def setUp(self):
+		AlgorithmTestCase.setUp(self)
+		self.algo = CuckooSearch
+
 	def test_custom_works_fine(self):
-		cs_custom = CuckooSearch(N=20, seed=self.seed)
-		cs_customc = CuckooSearch(N=20, seed=self.seed)
-		AlgorithmTestCase.algorithm_run_test(self, cs_custom, cs_customc, MyBenchmark())
+		cs_custom = self.algo(NP=20, seed=self.seed)
+		cs_customc = self.algo(NP=20, seed=self.seed)
+		AlgorithmTestCase.test_algorithm_run(self, cs_custom, cs_customc, MyBenchmark())
 
 	def test_griewank_works_fine(self):
-		cs_griewank = CuckooSearch(N=10, seed=self.seed)
-		cs_griewankc = CuckooSearch(N=10, seed=self.seed)
-		AlgorithmTestCase.algorithm_run_test(self, cs_griewank, cs_griewankc)
+		cs_griewank = self.algo(NP=10, seed=self.seed)
+		cs_griewankc = self.algo(NP=10, seed=self.seed)
+		AlgorithmTestCase.test_algorithm_run(self, cs_griewank, cs_griewankc)
 
 # vim: tabstop=3 noexpandtab shiftwidth=3 softtabstop=3
