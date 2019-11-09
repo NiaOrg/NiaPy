264121937c79bf7316a36fdab9d87ba1d19f8edf
Klemen@Haxor <roxor1992@gmail.com>
2019-11-09 11:57:47 +0100

    Added fixes for HC algorithm for new interface

--- NiaPy/algorithms/other/hc.py
+++ NiaPy/algorithms/other/hc.py
@@ -87,7 +87,14 @@ class HillClimbAlgorithm(Algorithm):
 		"""
 		Algorithm.setParameters(self, NP=1, **ukwargs)
 		self.delta, self.Neighborhood = delta, Neighborhood
-		if ukwargs: logger.info('Unused arguments: %s' % (ukwargs))
+
+	def getParameters(self):
+		d = Algorithm.getParameters(self)
+		d.update({
+			'delta': self.delta,
+			'Neighborhood': self.Neighborhood
+		})
+		return d
 
 	def initPopulation(self, task):
 		r"""Initialize stating point.
@@ -116,7 +123,7 @@ class HillClimbAlgorithm(Algorithm):
 			**dparams (Dict[str, Any]): Additional arguments.
 
 		Returns:
-			Tuple[numpy.ndarray, float, Dict[str, Any]]:
+			Tuple[numpy.ndarray, float, numpy.ndarray, float, Dict[str, Any]]:
 				1. New solution.
 				2. New solutions function/fitness value.
 				3. Additional arguments.
@@ -127,6 +134,7 @@ class HillClimbAlgorithm(Algorithm):
 			yn, yn_f = self.Neighborhood(x, self.delta, task, rnd=self.Rand)
 			if yn_f < xn_f: xn, xn_f = yn, yn_f
 			else: lo = True or task.stopCond()
-		return xn, xn_f, {}
+		xb, fxb = self.getBest(xn, xn_f, xb, fxb)
+		return xn, xn_f, xb, fxb, {}
 
 # vim: tabstop=3 noexpandtab shiftwidth=3 softtabstop=3
--- NiaPy/tests/test_hc.py
+++ NiaPy/tests/test_hc.py
@@ -4,32 +4,18 @@ from NiaPy.tests.test_algorithm import AlgorithmTestCase, MyBenchmark
 from NiaPy.algorithms.other import HillClimbAlgorithm
 
 class HCTestCase(AlgorithmTestCase):
-	r"""Test case for HillClimbAlgorithm algorithm.
-
-	Date:
-		April 2019
-
-	Author:
-		Klemen Berkovič and Jan Popič
-
-	See Also:
-		* :class:`NiaPy.algorithms.other.HillClimbAlgorithm`
-	"""
-	def test_algorithm_info_fine(self):
-		self.assertIsNotNone(HillClimbAlgorithm.algorithmInfo())
-
-	def test_type_parameters_fine(self):
-		d = HillClimbAlgorithm.typeParameters()
-		self.assertIsNotNone(d.get('delta', None))
+	def setUp(self):
+		AlgorithmTestCase.setUp(self)
+		self.algo = HillClimbAlgorithm
 
 	def test_custom_works_fine(self):
-		ihc_custom = HillClimbAlgorithm(delta=0.4, seed=self.seed)
-		ihc_customc = HillClimbAlgorithm(delta=0.4, seed=self.seed)
-		AlgorithmTestCase.algorithm_run_test(self, ihc_custom, ihc_customc, MyBenchmark())
+		ihc_custom = self.algo(delta=0.4, seed=self.seed)
+		ihc_customc = self.algo(delta=0.4, seed=self.seed)
+		AlgorithmTestCase.test_algorithm_run(self, ihc_custom, ihc_customc, MyBenchmark())
 
 	def test_griewank_works_fine(self):
-		ihc_griewank = HillClimbAlgorithm(delta=0.1, seed=self.seed)
-		ihc_griewankc = HillClimbAlgorithm(delta=0.1, seed=self.seed)
-		AlgorithmTestCase.algorithm_run_test(self, ihc_griewank, ihc_griewankc)
+		ihc_griewank = self.algo(delta=0.1, seed=self.seed)
+		ihc_griewankc = self.algo(delta=0.1, seed=self.seed)
+		AlgorithmTestCase.test_algorithm_run(self, ihc_griewank, ihc_griewankc)
 
 # vim: tabstop=3 noexpandtab shiftwidth=3 softtabstop=3
