7fb43b1abaf942d90783bd652e3d7c44babd4c8c
Klemen@Haxor <roxor1992@gmail.com>
2019-11-09 14:50:06 +0100

    Added fixes for FOA algorithm for the new interface

--- NiaPy/algorithms/basic/foa.py
+++ NiaPy/algorithms/basic/foa.py
@@ -12,237 +12,253 @@ logger = logging.getLogger('NiaPy.algorithms.basic')
 logger.setLevel('INFO')
 
 class ForestOptimizationAlgorithm(Algorithm):
-    r"""Implementation of Forest Optimization Algorithm.
+	r"""Implementation of Forest Optimization Algorithm.
 
-    Algorithm:
-        Forest Optimization Algorithm
+	Algorithm:
+		 Forest Optimization Algorithm
 
-    Date:
-        2019
+	Date:
+		 2019
 
-    Authors:
-        Luka Pečnik
+	Authors:
+		 Luka Pečnik
 
-    License:
-        MIT
+	License:
+		 MIT
 
-    Reference paper:
-        Manizheh Ghaemi, Mohammad-Reza Feizi-Derakhshi, Forest Optimization Algorithm, Expert Systems with Applications, Volume 41, Issue 15, 2014, Pages 6676-6687, ISSN 0957-4174, https://doi.org/10.1016/j.eswa.2014.05.009.
+	Reference paper:
+		 Manizheh Ghaemi, Mohammad-Reza Feizi-Derakhshi, Forest Optimization Algorithm, Expert Systems with Applications, Volume 41, Issue 15, 2014, Pages 6676-6687, ISSN 0957-4174, https://doi.org/10.1016/j.eswa.2014.05.009.
 
-    References URL:
-        Implementation is based on the following MATLAB code: https://github.com/cominsys/FOA
+	References URL:
+		 Implementation is based on the following MATLAB code: https://github.com/cominsys/FOA
 
-    Attributes:
-        Name (List[str]): List of strings representing algorithm name.
-        lt (int): Life time of trees parameter.
-        al (int): Area limit parameter.
-        lsc (int): Local seeding changes parameter.
-        gsc (int): Global seeding changes parameter.
-        tr (float): Transfer rate parameter.
+	Attributes:
+		 Name (List[str]): List of strings representing algorithm name.
+		 lt (int): Life time of trees parameter.
+		 al (int): Area limit parameter.
+		 lsc (int): Local seeding changes parameter.
+		 gsc (int): Global seeding changes parameter.
+		 tr (float): Transfer rate parameter.
 
-    See Also:
-        * :class:`NiaPy.algorithms.Algorithm`
-    """
-    Name = ['ForestOptimizationAlgorithm', 'FOA']
+	See Also:
+		 * :class:`NiaPy.algorithms.Algorithm`
+	"""
+	Name = ['ForestOptimizationAlgorithm', 'FOA']
 
-    @staticmethod
-    def algorithmInfo():
-        return r"""
+	@staticmethod
+	def algorithmInfo():
+		r"""Get algorithms information.
+
+		Returns:
+			str: Algorithm information.
+		"""
+		return r"""
         Description: Forest Optimization Algorithm is inspired by few trees in the forests which can survive for several decades, while other trees could live for a limited period.
         Authors: Manizheh Ghaemi, Mohammad-Reza Feizi-Derakhshi
         Year: 2014
         Main reference: Manizheh Ghaemi, Mohammad-Reza Feizi-Derakhshi, Forest Optimization Algorithm, Expert Systems with Applications, Volume 41, Issue 15, 2014, Pages 6676-6687, ISSN 0957-4174, https://doi.org/10.1016/j.eswa.2014.05.009.
-    """
-
-    @staticmethod
-    def typeParameters():
-        r"""Get dictionary with functions for checking values of parameters.
-
-        Returns:
-            Dict[str, Callable]:
-                * lt (Callable[[int], bool]): Checks if life time parameter has a proper value.
-                * al (Callable[[int], bool]): Checks if area limit parameter has a proper value.
-                * lsc (Callable[[int], bool]): Checks if local seeding changes parameter has a proper value.
-                * gsc (Callable[[int], bool]): Checks if global seeding changes parameter has a proper value.
-                * tr (Callable[[float], bool]): Checks if transfer rate parameter has a proper value.
-
-        See Also:
-            * :func:`NiaPy.algorithms.algorithm.Algorithm.typeParameters`
         """
-        d = Algorithm.typeParameters()
-        d.update({
-            'lt': lambda x: isinstance(x, int) and x > 0,
-            'al': lambda x: isinstance(x, int) and x > 0,
-            'lsc': lambda x: isinstance(x, int) and x > 0,
-            'gsc': lambda x: isinstance(x, int) and x > 0,
-            'tr': lambda x: isinstance(x, float) and 0 <= x <= 1,
-        })
-        return d
 
-    def setParameters(self, NP=10, lt=3, al=10, lsc=1, gsc=1, tr=0.3, **ukwargs):
-        r"""Set the parameters of the algorithm.
+	@staticmethod
+	def typeParameters():
+		r"""Get dictionary with functions for checking values of parameters.
 
-        Args:
-            NP (Optional[int]): Population size.
-            lt (Optional[int]): Life time parameter.
-            al (Optional[int]): Area limit parameter.
-            lsc (Optional[int]): Local seeding changes parameter.
-            gsc (Optional[int]): Global seeding changes parameter.
-            tr (Optional[float]): Transfer rate parameter.
-            ukwargs (Dict[str, Any]): Additional arguments.
+		Returns:
+			 Dict[str, Callable]:
+				  * lt (Callable[[int], bool]): Checks if life time parameter has a proper value.
+				  * al (Callable[[int], bool]): Checks if area limit parameter has a proper value.
+				  * lsc (Callable[[int], bool]): Checks if local seeding changes parameter has a proper value.
+				  * gsc (Callable[[int], bool]): Checks if global seeding changes parameter has a proper value.
+				  * tr (Callable[[float], bool]): Checks if transfer rate parameter has a proper value.
 
-        See Also:
-            * :func:`NiaPy.algorithms.Algorithm.setParameters`
-        """
-        Algorithm.setParameters(self, NP=NP)
-        self.lt, self.al, self.lsc, self.gsc, self.tr = lt, al, lsc, gsc, tr
-        if ukwargs: logger.info('Unused arguments: %s' % (ukwargs))
+		See Also:
+			 * :func:`NiaPy.algorithms.algorithm.Algorithm.typeParameters`
+		"""
+		d = Algorithm.typeParameters()
+		d.update({
+			'lt': lambda x: isinstance(x, int) and x > 0,
+			'al': lambda x: isinstance(x, int) and x > 0,
+			'lsc': lambda x: isinstance(x, int) and x > 0,
+			'gsc': lambda x: isinstance(x, int) and x > 0,
+			'tr': lambda x: isinstance(x, float) and 0 <= x <= 1,
+		})
+		return d
 
-    def localSeeding(self, task, trees):
-        r"""Local optimum search stage.
+	def setParameters(self, NP=10, lt=3, al=10, lsc=1, gsc=1, tr=0.3, **ukwargs):
+		r"""Set the parameters of the algorithm.
 
-        Args:
-            task (Task): Optimization task.
-            trees (numpy.ndarray): Zero age trees for local seeding.
+		Args:
+			 NP (Optional[int]): Population size.
+			 lt (Optional[int]): Life time parameter.
+			 al (Optional[int]): Area limit parameter.
+			 lsc (Optional[int]): Local seeding changes parameter.
+			 gsc (Optional[int]): Global seeding changes parameter.
+			 tr (Optional[float]): Transfer rate parameter.
+			 ukwargs (Dict[str, Any]): Additional arguments.
 
-        Returns:
-            numpy.ndarray: Resulting zero age trees.
-        """
-        n = trees.shape[0]
-        deltas = self.uniform(-self.dx, self.dx, (n, self.lsc))
-        deltas = append(deltas, zeros((n, task.D - self.lsc)), axis=1)
-        perms = self.rand([deltas.shape[0], deltas.shape[1]]).argsort(1)
-        deltas = deltas[arange(deltas.shape[0])[:, None], perms]
-        trees += deltas
-        trees = apply_along_axis(limit_repair, 1, trees, task.Lower, task.Upper)
-        return trees
+		See Also:
+			 * :func:`NiaPy.algorithms.Algorithm.setParameters`
+		"""
+		Algorithm.setParameters(self, NP=NP, **ukwargs)
+		self.lt, self.al, self.lsc, self.gsc, self.tr = lt, al, lsc, gsc, tr
 
-    def globalSeeding(self, task, candidates, size):
-        r"""Global optimum search stage that should prevent getting stuck in a local optimum.
+	def getParameters(self):
+		r"""Get parameters values of the algorithm.
 
-        Args:
-            task (Task): Optimization task.
-            candidates (numpy.ndarray): Candidate population for global seeding.
-            size (int): Number of trees to produce.
+		Returns:
+			Dict[str, Any]: TODO.
+		"""
+		d = Algorithm.getParameters(self)
+		d.update({
+			'lt': self.lt,
+			'al': self.al,
+			'lsc': self.lsc,
+			'gsc': self.gsc,
+			'tr': self.tr
+		})
+		return d
 
-        Returns:
-            numpy.ndarray: Resulting trees.
-        """
-        seeds = candidates[self.randint(len(candidates), D=size)]
-        deltas = self.uniform(task.benchmark.Lower, task.benchmark.Upper, (size, self.gsc))
-        deltas = append(deltas, zeros((size, task.D - self.gsc)), axis=1)
-        perms = self.rand([deltas.shape[0], deltas.shape[1]]).argsort(1)
-        deltas = deltas[arange(deltas.shape[0])[:, None], perms]
+	def localSeeding(self, task, trees):
+		r"""Local optimum search stage.
 
-        deltas = deltas.flatten()
-        seeds = seeds.flatten()
-        seeds[deltas != 0] = deltas[deltas != 0]
+		Args:
+			 task (Task): Optimization task.
+			 trees (numpy.ndarray): Zero age trees for local seeding.
 
-        return seeds.reshape(size, task.D)
+		Returns:
+			 numpy.ndarray: Resulting zero age trees.
+		"""
+		n = trees.shape[0]
+		deltas = self.uniform(-self.dx, self.dx, (n, self.lsc))
+		deltas = append(deltas, zeros((n, task.D - self.lsc)), axis=1)
+		perms = self.rand([deltas.shape[0], deltas.shape[1]]).argsort(1)
+		deltas = deltas[arange(deltas.shape[0])[:, None], perms]
+		trees += deltas
+		trees = apply_along_axis(limit_repair, 1, trees, task.Lower, task.Upper)
+		return trees
 
-    def removeLifeTimeExceeded(self, trees, candidates, age):
-        r"""Remove dead trees.
+	def globalSeeding(self, task, candidates, size):
+		r"""Global optimum search stage that should prevent getting stuck in a local optimum.
 
-        Args:
-            trees (numpy.ndarray): Population to test.
-            candidates (numpy.ndarray): Candidate population array to be updated.
-            age (numpy.ndarray[int32]): Age of trees.
+		Args:
+			 task (Task): Optimization task.
+			 candidates (numpy.ndarray): Candidate population for global seeding.
+			 size (int): Number of trees to produce.
 
-        Returns:
-            Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray[int32]]:
-                1. Alive trees.
-                2. New candidate population.
-                3. Age of trees.
-        """
-        lifeTimeExceeded = where(age > self.lt)
-        candidates = trees[lifeTimeExceeded]
-        trees = delete(trees, lifeTimeExceeded, axis=0)
-        age = delete(age, lifeTimeExceeded, axis=0)
-        return trees, candidates, age
+		Returns:
+			 numpy.ndarray: Resulting trees.
+		"""
+		seeds = candidates[self.randint(len(candidates), D=size)]
+		deltas = self.uniform(task.benchmark.Lower, task.benchmark.Upper, (size, self.gsc))
+		deltas = append(deltas, zeros((size, task.D - self.gsc)), axis=1)
+		perms = self.rand([deltas.shape[0], deltas.shape[1]]).argsort(1)
+		deltas = deltas[arange(deltas.shape[0])[:, None], perms]
+		deltas = deltas.flatten()
+		seeds = seeds.flatten()
+		seeds[deltas != 0] = deltas[deltas != 0]
 
-    def survivalOfTheFittest(self, task, trees, candidates, age):
-        r"""Evaluate and filter current population.
+		return seeds.reshape(size, task.D)
 
-        Args:
-            task (Task): Optimization task.
-            trees (numpy.ndarray): Population to evaluate.
-            candidates (numpy.ndarray): Candidate population array to be updated.
-            age (numpy.ndarray[int32]): Age of trees.
+	def removeLifeTimeExceeded(self, trees, candidates, age):
+		r"""Remove dead trees.
 
-        Returns:
-            Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray[float], numpy.ndarray[int32]]:
-                1. Trees sorted by fitness value.
-                2. Updated candidate population.
-                3. Population fitness values.
-                4. Age of trees
-        """
-        evaluations = apply_along_axis(task.eval, 1, trees)
-        ei = evaluations.argsort()
-        candidates = append(candidates, trees[ei[self.al:]], axis=0)
-        trees = trees[ei[:self.al]]
-        age = age[ei[:self.al]]
-        evaluations = evaluations[ei[:self.al]]
-        return trees, candidates, evaluations, age
+		Args:
+			 trees (numpy.ndarray): Population to test.
+			 candidates (numpy.ndarray): Candidate population array to be updated.
+			 age (numpy.ndarray[int32]): Age of trees.
 
-    def initPopulation(self, task):
-        r"""Initialize the starting population.
+		Returns:
+			 Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray[int32]]:
+				  1. Alive trees.
+				  2. New candidate population.
+				  3. Age of trees.
+		"""
+		lifeTimeExceeded = where(age > self.lt)
+		candidates = trees[lifeTimeExceeded]
+		trees = delete(trees, lifeTimeExceeded, axis=0)
+		age = delete(age, lifeTimeExceeded, axis=0)
+		return trees, candidates, age
 
-        Args:
-            task (Task): Optimization task
+	def survivalOfTheFittest(self, task, trees, candidates, age):
+		r"""Evaluate and filter current population.
 
-        Returns:
-            Tuple[numpy.ndarray, numpy.ndarray[float], Dict[str, Any]]:
-                1. New population.
-                2. New population fitness/function values.
-                3. Additional arguments:
-                    * age (numpy.ndarray[int32]): Age of trees.
+		Args:
+			 task (Task): Optimization task.
+			 trees (numpy.ndarray): Population to evaluate.
+			 candidates (numpy.ndarray): Candidate population array to be updated.
+			 age (numpy.ndarray[int32]): Age of trees.
 
-        See Also:
-            * :func:`NiaPy.algorithms.Algorithm.initPopulation`
-        """
-        Trees, Evaluations, _ = Algorithm.initPopulation(self, task)
-        age = zeros(self.NP, dtype=int32)
-        self.dx = absolute(task.benchmark.Upper) / 5
-        return Trees, Evaluations, {'age': age}
+		Returns:
+			 Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray[float], numpy.ndarray[int32]]:
+				  1. Trees sorted by fitness value.
+				  2. Updated candidate population.
+				  3. Population fitness values.
+				  4. Age of trees
+		"""
+		evaluations = apply_along_axis(task.eval, 1, trees)
+		ei = evaluations.argsort()
+		candidates = append(candidates, trees[ei[self.al:]], axis=0)
+		trees = trees[ei[:self.al]]
+		age = age[ei[:self.al]]
+		evaluations = evaluations[ei[:self.al]]
+		return trees, candidates, evaluations, age
 
-    def runIteration(self, task, Trees, Evaluations, xb, fxb, age, **dparams):
-        r"""Core function of Forest Optimization Algorithm.
+	def initPopulation(self, task):
+		r"""Initialize the starting population.
 
-        Args:
-            task (Task): Optimization task.
-            Trees (numpy.ndarray): Current population.
-            Evaluations (numpy.ndarray[float]): Current population function/fitness values.
-            xb (numpy.ndarray): Global best individual.
-            fxb (float): Global best individual fitness/function value.
-            age (numpy.ndarray[int32]): Age of trees.
-            **dparams (Dict[str, Any]): Additional arguments.
+		Args:
+			 task (Task): Optimization task
 
-        Returns:
-            Tuple[numpy.ndarray, numpy.ndarray[float], Dict[str, Any]]:
-                1. New population.
-                2. New population fitness/function values.
-                3. Additional arguments:
-                    * age (numpy.ndarray[int32]): Age of trees.
-        """
-        candidatePopulation = ndarray((0, task.D + 1))
-        zeroAgeTrees = Trees[age == 0]
+		Returns:
+			 Tuple[numpy.ndarray, numpy.ndarray[float], Dict[str, Any]]:
+				  1. New population.
+				  2. New population fitness/function values.
+				  3. Additional arguments:
+						* age (numpy.ndarray[int32]): Age of trees.
 
-        localSeeds = self.localSeeding(task, zeroAgeTrees)
-        age += 1
+		See Also:
+			 * :func:`NiaPy.algorithms.Algorithm.initPopulation`
+		"""
+		Trees, Evaluations, _ = Algorithm.initPopulation(self, task)
+		age = zeros(self.NP, dtype=int32)
+		self.dx = absolute(task.benchmark.Upper) / 5
+		return Trees, Evaluations, {'age': age}
 
-        Trees, candidatePopulation, age = self.removeLifeTimeExceeded(Trees, candidatePopulation, age)
-        Trees = append(Trees, localSeeds, axis=0)
-        age = append(age, zeros(len(localSeeds), dtype=int32))
-        Trees, candidatePopulation, Evaluations, age = self.survivalOfTheFittest(task, Trees, candidatePopulation, age)
+	def runIteration(self, task, Trees, Evaluations, xb, fxb, age, **dparams):
+		r"""Core function of Forest Optimization Algorithm.
 
-        gsn = int(self.tr * len(candidatePopulation))
-        if gsn > 0:
-            globalSeeds = self.globalSeeding(task, candidatePopulation, gsn)
-            Trees = append(Trees, globalSeeds, axis=0)
-            age = append(age, zeros(len(globalSeeds), dtype=int32))
-            gste = apply_along_axis(task.eval, 1, globalSeeds)
-            Evaluations = append(Evaluations, gste)
+		Args:
+			 task (Task): Optimization task.
+			 Trees (numpy.ndarray): Current population.
+			 Evaluations (numpy.ndarray[float]): Current population function/fitness values.
+			 xb (numpy.ndarray): Global best individual.
+			 fxb (float): Global best individual fitness/function value.
+			 age (numpy.ndarray[int32]): Age of trees.
+			 **dparams (Dict[str, Any]): Additional arguments.
 
-        ib = argmin(Evaluations)
-        age[ib] = 0
-        return Trees, Evaluations, {'age': age}
+		Returns:
+			 Tuple[numpy.ndarray, numpy.ndarray[float], Dict[str, Any]]:
+				  1. New population.
+				  2. New population fitness/function values.
+				  3. Additional arguments:
+						* age (numpy.ndarray[int32]): Age of trees.
+		"""
+		candidatePopulation = ndarray((0, task.D + 1))
+		zeroAgeTrees = Trees[age == 0]
+		localSeeds = self.localSeeding(task, zeroAgeTrees)
+		age += 1
+		Trees, candidatePopulation, age = self.removeLifeTimeExceeded(Trees, candidatePopulation, age)
+		Trees = append(Trees, localSeeds, axis=0)
+		age = append(age, zeros(len(localSeeds), dtype=int32))
+		Trees, candidatePopulation, Evaluations, age = self.survivalOfTheFittest(task, Trees, candidatePopulation, age)
+		gsn = int(self.tr * len(candidatePopulation))
+		if gsn > 0:
+			globalSeeds = self.globalSeeding(task, candidatePopulation, gsn)
+			Trees = append(Trees, globalSeeds, axis=0)
+			age = append(age, zeros(len(globalSeeds), dtype=int32))
+			gste = apply_along_axis(task.eval, 1, globalSeeds)
+			Evaluations = append(Evaluations, gste)
+		ib = argmin(Evaluations)
+		age[ib] = 0
+		if Evaluations[ib] < fxb: xb, fxb = Trees[ib].copy(), Evaluations[ib]
+		return Trees, Evaluations, xb, fxb, {'age': age}
--- NiaPy/tests/test_foa.py
+++ NiaPy/tests/test_foa.py
@@ -1,43 +1,49 @@
-# pylint: disable=line-too-long
+# encoding=utf8
+# pylint: disable=line-too-long, mixed-indentation
 from NiaPy.tests.test_algorithm import AlgorithmTestCase, MyBenchmark
 from NiaPy.algorithms.basic import ForestOptimizationAlgorithm
 
 class FOATestCase(AlgorithmTestCase):
+	def setUp(self):
+		AlgorithmTestCase.setUp(self)
+		self.algo = ForestOptimizationAlgorithm
 
-    def test_type_parameters(self):
-        tp = ForestOptimizationAlgorithm.typeParameters()
-        self.assertTrue(tp['NP'](1))
-        self.assertFalse(tp['NP'](0))
-        self.assertFalse(tp['NP'](-1))
-        self.assertFalse(tp['NP'](1.0))
-        self.assertTrue(tp['lt'](1))
-        self.assertFalse(tp['lt'](0))
-        self.assertFalse(tp['lt'](-1))
-        self.assertFalse(tp['lt'](1.0))
-        self.assertTrue(tp['al'](1))
-        self.assertFalse(tp['al'](0))
-        self.assertFalse(tp['al'](-1))
-        self.assertFalse(tp['al'](1.0))
-        self.assertTrue(tp['lsc'](1))
-        self.assertFalse(tp['lsc'](0))
-        self.assertFalse(tp['lsc'](-1))
-        self.assertFalse(tp['lsc'](1.0))
-        self.assertTrue(tp['gsc'](1))
-        self.assertFalse(tp['gsc'](0))
-        self.assertFalse(tp['gsc'](-1))
-        self.assertFalse(tp['gsc'](1.0))
-        self.assertTrue(tp['tr'](1.0))
-        self.assertTrue(tp['tr'](0.5))
-        self.assertTrue(tp['tr'](0.0))
-        self.assertFalse(tp['tr'](-1))
-        self.assertFalse(tp['tr'](1.1))
+	def test_type_parameters(self):
+		tp = self.algo.typeParameters()
+		self.assertTrue(tp['NP'](1))
+		self.assertFalse(tp['NP'](0))
+		self.assertFalse(tp['NP'](-1))
+		self.assertFalse(tp['NP'](1.0))
+		self.assertTrue(tp['lt'](1))
+		self.assertFalse(tp['lt'](0))
+		self.assertFalse(tp['lt'](-1))
+		self.assertFalse(tp['lt'](1.0))
+		self.assertTrue(tp['al'](1))
+		self.assertFalse(tp['al'](0))
+		self.assertFalse(tp['al'](-1))
+		self.assertFalse(tp['al'](1.0))
+		self.assertTrue(tp['lsc'](1))
+		self.assertFalse(tp['lsc'](0))
+		self.assertFalse(tp['lsc'](-1))
+		self.assertFalse(tp['lsc'](1.0))
+		self.assertTrue(tp['gsc'](1))
+		self.assertFalse(tp['gsc'](0))
+		self.assertFalse(tp['gsc'](-1))
+		self.assertFalse(tp['gsc'](1.0))
+		self.assertTrue(tp['tr'](1.0))
+		self.assertTrue(tp['tr'](0.5))
+		self.assertTrue(tp['tr'](0.0))
+		self.assertFalse(tp['tr'](-1))
+		self.assertFalse(tp['tr'](1.1))
 
-    def test_works_fine(self):
-        foa = ForestOptimizationAlgorithm(NP=20, lt=5, lsc=1, gsc=1, al=20, tr=0.35, seed=self.seed)
-        foac = ForestOptimizationAlgorithm(NP=20, lt=5, lsc=1, gsc=1, al=20, tr=0.35, seed=self.seed)
-        AlgorithmTestCase.algorithm_run_test(self, foa, foac, MyBenchmark())
+	def test_works_fine(self):
+		foa = self.algo(NP=20, lt=5, lsc=1, gsc=1, al=20, tr=0.35, seed=self.seed)
+		foac = self.algo(NP=20, lt=5, lsc=1, gsc=1, al=20, tr=0.35, seed=self.seed)
+		AlgorithmTestCase.test_algorithm_run(self, foa, foac, MyBenchmark())
 
-    def test_griewank_works_fine(self):
-        foa_griewank = ForestOptimizationAlgorithm(NP=20, lt=5, lsc=1, gsc=1, al=20, tr=0.35, seed=self.seed)
-        foa_griewankc = ForestOptimizationAlgorithm(NP=20, lt=5, lsc=1, gsc=1, al=20, tr=0.35, seed=self.seed)
-        AlgorithmTestCase.algorithm_run_test(self, foa_griewank, foa_griewankc)
+	def test_griewank_works_fine(self):
+		foa_griewank = self.algo(NP=20, lt=5, lsc=1, gsc=1, al=20, tr=0.35, seed=self.seed)
+		foa_griewankc = self.algo(NP=20, lt=5, lsc=1, gsc=1, al=20, tr=0.35, seed=self.seed)
+		AlgorithmTestCase.test_algorithm_run(self, foa_griewank, foa_griewankc)
+
+# vim: tabstop=3 noexpandtab shiftwidth=3 softtabstop=3
