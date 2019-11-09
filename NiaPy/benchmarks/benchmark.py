0f2ede3559c76547c5c88575433c0a7337cc50df
Klemen@Haxor <roxor1992@gmail.com>
2019-11-09 12:35:58 +0100

    Added fixes for benchmark

--- NiaPy/benchmarks/benchmark.py
+++ NiaPy/benchmarks/benchmark.py
@@ -1,105 +1,90 @@
 # encoding=utf8
-
-"""Implementation of base benchmark class."""
-
+# pylint: disable=mixed-indentation, line-too-long, bad-continuation, multiple-statements, singleton-comparison, unused-argument, no-self-use, trailing-comma-tuple, logging-not-lazy, no-else-return, unused-variable, no-member
+"""Implementation of benchmarks utility function."""
 import logging
 from numpy import inf, arange, meshgrid, vectorize
 from matplotlib import pyplot as plt
 from matplotlib import cm
 
 logging.basicConfig()
-logger = logging.getLogger("NiaPy.benchmarks.benchmark")
-logger.setLevel("INFO")
+logger = logging.getLogger('NiaPy.benchmarks.benchmark')
+logger.setLevel('INFO')
 
-__all__ = ["Benchmark"]
+__all__ = ['Benchmark']
 
-
-class Benchmark(object):
-    r"""Class representing benchmarks.
-
-    Attributes:
-            Name (List[str]): List of names representiong benchmark names.
-            Lower (Union[int, float, list, numpy.ndarray]): Lower bounds.
-            Upper (Union[int, float, list, numpy.ndarray]): Upper bounds.
-
+class Benchmark:
+	r"""Class representing benchmarks.
+	
+	Attributes:
+		Name (List[str]): List of names representiong benchmark names.
+		Lower (Union[int, float, list, numpy.ndarray]): Lower bounds.
+		Upper (Union[int, float, list, numpy.ndarray]): Upper bounds.
     """
+	Name = ['Benchmark', 'BBB']
 
-    Name = ["Benchmark", "BBB"]
+	def __init__(self, Lower, Upper, **kwargs):
+		r"""Initialize benchmark.
+		
+		Args:
+			Lower (Union[int, float, list, numpy.ndarray]): Lower bounds.
+			Upper (Union[int, float, list, numpy.ndarray]): Upper bounds.
+			**kwargs (Dict[str, Any]): Additional arguments.
+		"""
+		self.Lower, self.Upper = Lower, Upper
 
-    def __init__(self, Lower, Upper, **kwargs):
-        r"""Initialize benchmark.
+	def function(self):
+		r"""Get the optimization function.
+		
+		Returns:
+			Callable[[int, Union[list, numpy.ndarray]], float]: Fitness funciton.
+		"""
+		def fun(D, X):
+			r"""Initialize benchmark.
+			
+			Args:
+				D (int): Dimesionality of the problem.
+				X (Union[int, float, list, numpy.ndarray]): Solution to the problem.
+			
+			Retruns:
+				float: Fitness value for the solution
+			"""
+			return inf
+		return fun
 
-        Args:
-                Lower (Union[int, float, list, numpy.ndarray]): Lower bounds.
-                Upper (Union[int, float, list, numpy.ndarray]): Upper bounds.
-                **kwargs (Dict[str, Any]): Additional arguments.
+	def plot2d(self):
+		r"""Plot 2D graph."""
+		pass
 
-        """
+	def __2dfun(self, x, y, f):
+		r"""Calculate function value.
+		
+		Args:
+			x (float): First coordinate.
+			y (float): Second coordinate.
+			f (Callable[[int, Union[int, float, List[int, float], numpy.ndarray]], float]): Evaluation function.
+		
+		Returns:
+			float: Calculate functional value for given input
+		"""
+		return f(2, x, y)
 
-        self.Lower = Lower
-        self.Upper = Upper
+	def plot3d(self, scale=0.32):
+		r"""Plot 3d scatter plot of benchmark function.
+		
+		Args:
+			scale (float): Scale factor for points.
+		"""
+		fig = plt.figure()
+		ax = fig.gca(projection='3d')
+		func = self.function()
+		Xr, Yr = arange(self.Lower, self.Upper, scale), arange(self.Lower, self.Upper, scale)
+		X, Y = meshgrid(Xr, Yr)
+		Z = vectorize(self.__2dfun)(X, Y, func)
+		ax.plot_surface(X, Y, Z, rstride=8, cstride=8, alpha=0.3)
+		cset = ax.contourf(X, Y, Z, zdir='z', offset=-10, cmap=cm.coolwarm)
+		ax.set_xlabel('X')
+		ax.set_ylabel('Y')
+		ax.set_zlabel('Z')
+		plt.show()
 
-    def function(self):
-        r"""Get evaluation function.
-
-        Returns:
-                Callable[[int, Union[list, numpy.ndarray]], float]): Evaluation function.
-
-        """
-
-        def evaluate(D, sol):
-            r"""Utility/Evaluation function.
-
-            Args:
-                    D (int): Number of coordinates.
-                    sol (Union[list, numpy.ndarray]): Solution to evaluate.
-
-            Returns:
-                    float: Function value.
-
-            """
-
-            return inf
-
-        return evaluate
-
-    def plot2d(self):
-        """Plot."""
-        pass
-
-    def __2dfun(self, x, y, f):
-        r"""Calculate function value.
-
-        Args:
-                x (float): First coordinate.
-                y (float): Second coordinate.
-                f (Callable[[int, List[float]], float]): Evaluation function.
-
-        Returns:
-                float: Calculate functional value for given input
-
-        """
-
-        return f(2, x, y)
-
-    def plot3d(self, scale=0.32):
-        r"""Plot 3d scatter plot of benchmark function.
-
-        Args:
-                scale (float): Scale factor for points.
-
-        """
-
-        fig = plt.figure()
-        ax = fig.gca(projection="3d")
-        func = self.function()
-        Xr, Yr = arange(self.Lower, self.Upper, scale), arange(
-            self.Lower, self.Upper, scale)
-        X, Y = meshgrid(Xr, Yr)
-        Z = vectorize(self.__2dfun)(X, Y, func)
-        ax.plot_surface(X, Y, Z, rstride=8, cstride=8, alpha=0.3)
-        ax.contourf(X, Y, Z, zdir="z", offset=-10, cmap=cm.coolwarm)
-        ax.set_xlabel("X")
-        ax.set_ylabel("Y")
-        ax.set_zlabel("Z")
-        plt.show()
+# vim: tabstop=3 noexpandtab shiftwidth=3 softtabstop=3
