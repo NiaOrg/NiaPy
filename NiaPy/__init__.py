1ffd79647e71974be1272118fcb78c56155b5399
Grega Vrbančič <grega.vrbancic@gmail.com>
2019-11-12 09:02:30 +0100

    Bump version: 2.0.0rc9 → 2.0.0rc10

--- NiaPy/__init__.py
+++ NiaPy/__init__.py
@@ -10,6 +10,6 @@ from NiaPy.runner import Runner
 
 __all__ = ["algorithms", "benchmarks", "util", "task", "Runner"]
 __project__ = "NiaPy"
-__version__ = "2.0.0rc9"
+__version__ = "2.0.0rc10"
 
 VERSION = "{0} v{1}".format(__project__, __version__)
