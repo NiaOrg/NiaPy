46e0ae71b3617fa0a890fbe0fcd7fbf547bbc96a
Grega Vrbančič <grega.vrbancic@gmail.com>
2020-01-13 17:35:38 +0100

    Release dependencies version

--- setup.py
+++ setup.py
@@ -73,10 +73,9 @@ setuptools.setup(
     ],
 
     install_requires=[
-        # TODO: Add your library's requirements here
-        'numpy ~= 1.14.0',
-        'click ~= 6.0',
+        'numpy >= 1.14.0',
+        'click >= 6.0',
         'scipy >= 1.0.0',
-        'xlsxwriter ~= 1.0.2'
+        'xlsxwriter >= 1.0.2'
     ]
 )
