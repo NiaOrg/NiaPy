35c2603e9dd3a59627999b56ef48526277873416
firefly-cpp <pingvin1@gmail.com>
2020-02-12 13:05:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setuptools.setup(
     install_requires=[
         'numpy >= 1.14.0',
         'click >= 6.0',
-        'scipy >= 1.0.0',
+        'scipy >= 1.2.1',
         'xlsxwriter >= 1.0.2'
     ]
 )
