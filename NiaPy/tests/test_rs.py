f3155032cc0de0d75d378dd434b9ed67b9d44a3a
firefly-cpp <iztok@iztok-jr-fister.eu>
2020-10-17 11:54:49 +0200

    tests for RS algorithm

---
+++ NiaPy/tests/test_rs.py
@@ -0,0 +1,19 @@
+# encoding=utf8
+
+from NiaPy.tests.test_algorithm import AlgorithmTestCase, MyBenchmark
+from NiaPy.algorithms.other import RandomSearch
+
+class RSTestCase(AlgorithmTestCase):
+	def setUp(self):
+		AlgorithmTestCase.setUp(self)
+		self.algo = RandomSearch
+
+	def test_custom_works_fine(self):
+		ca_custom = self.algo(NP=40, seed=self.seed)
+		ca_customc = self.algo(NP=40, seed=self.seed)
+		AlgorithmTestCase.test_algorithm_run(self, ca_custom, ca_customc, MyBenchmark())
+
+	def test_griewank_works_fine(self):
+		ca_griewank = self.algo(NP=40, seed=self.seed)
+		ca_griewankc = self.algo(NP=40, seed=self.seed)
+		AlgorithmTestCase.test_algorithm_run(self, ca_griewank, ca_griewankc)
