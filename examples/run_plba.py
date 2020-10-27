df5d3039c9ebef70750d0251413811680d1a61ea
firefly-cpp <pingvin1@gmail.com>
2020-10-27 13:20:20 +0100

    Implementation of PLBA algorithm (#278)
    * Implementation of PLBA algorithm

---
+++ examples/run_plba.py
@@ -0,0 +1,19 @@
+# encoding=utf8
+# This is temporary fix to import module from parent folder
+# It will be removed when package is published on PyPI
+import sys
+sys.path.append('../')
+# End of fix
+
+from NiaPy.algorithms.modified import ParameterFreeBatAlgorithm
+
+from NiaPy.task import StoppingTask
+from NiaPy.benchmarks import Sphere
+
+algo = ParameterFreeBatAlgorithm()
+
+for i in range(10):
+	task = StoppingTask(D=10, nFES=10000, benchmark=Sphere(Upper=5.12, Lower=-5.12))
+	best = algo.run(task)
+	print('%s -> %s' % (best[0], best[1]))
+print(algo.getParameters())
