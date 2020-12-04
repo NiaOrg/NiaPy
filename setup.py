55fb52a7926e521dfd18f30c649d88735c143323
Grega Vrbančič <grega.vrbancic@gmail.com>
2020-12-04 11:17:39 +0100

    Bump version: 2.0.0rc11 → 2.0.0rc12

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ PACKAGE_VERSION = read_package_variable('__version__')
 
 setuptools.setup(
     name=PACKAGE_NAME,
-    version="2.0.0rc11",
+    version="2.0.0rc12",
     description="""
         Python micro framework for building nature-inspired algorithms.
         """,
