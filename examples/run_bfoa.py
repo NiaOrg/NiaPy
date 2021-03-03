fed0b204a2244af86732ec18e56257ce40aea095
zStupan <ziga.stupan@gmail.com>
2021-03-03 12:11:27 +0100

    Added BFOA example

---
+++ examples/run_bfoa.py
@@ -0,0 +1,17 @@
+# encoding=utf8
+# This is temporary fix to import module from parent folder
+# It will be removed when package is published on PyPI
+import sys
+sys.path.append('../')
+# End of fix
+
+from NiaPy.algorithms.basic import BacterialForagingOptimizationAlgorithm
+from NiaPy.task import StoppingTask
+from NiaPy.benchmarks import Griewank
+
+# we will run Bacterial Foraging Optimization Algorithm for 5 independent runs
+for i in range(5):
+    task = StoppingTask(D=10, nGEN=10000, benchmark=Griewank(Lower=-600, Upper=600), logger=True)
+    algo = BacterialForagingOptimizationAlgorithm()
+    best = algo.run(task)
+    print('%s -> %s' % (best[0], best[1]))
