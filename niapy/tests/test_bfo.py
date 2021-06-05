a6bdd5e4f3d2a7deec6042aff92dd2685f05f264
zStupan <ziga.stupan@gmail.com>
2021-06-05 22:31:31 +0200

    Added flag to enable/disable swarming

--- niapy/tests/test_bfo.py
+++ niapy/tests/test_bfo.py
@@ -12,29 +12,29 @@ class BFOTestCase(AlgorithmTestCase):
     def test_custom(self):
         bfoa_custom = self.algo(population_size=10, n_chemotactic=100, n_reproduction=4, n_elimination=1, step_size=0.1,
                                 seed=self.seed)
-        bfoa_customc = self.algo(population_size=10, n_chemotactic=100, n_reproduction=4, n_elimination=1, step_size=0.1,
-                                 seed=self.seed)
+        bfoa_customc = self.algo(population_size=10, n_chemotactic=100, n_reproduction=4, n_elimination=1,
+                                 step_size=0.1, seed=self.seed)
         AlgorithmTestCase.test_algorithm_run(self, bfoa_custom, bfoa_customc, MyProblem())
 
     def test_griewank(self):
         bfoa_custom = self.algo(population_size=10, n_chemotactic=100, n_reproduction=4, n_elimination=1, step_size=0.1,
                                 seed=self.seed)
-        bfoa_customc = self.algo(population_size=10, n_chemotactic=100, n_reproduction=4, n_elimination=1, step_size=0.1,
-                                 seed=self.seed)
+        bfoa_customc = self.algo(population_size=10, n_chemotactic=100, n_reproduction=4, n_elimination=1,
+                                 step_size=0.1, seed=self.seed)
         AlgorithmTestCase.test_algorithm_run(self, bfoa_custom, bfoa_customc)
 
     def test_griewank_nfes(self):
         bfoa_custom = self.algo(population_size=10, n_chemotactic=100, n_reproduction=4, n_elimination=1, step_size=0.1,
                                 seed=self.seed)
-        bfoa_customc = self.algo(population_size=10, n_chemotactic=100, n_reproduction=4, n_elimination=1, step_size=0.1,
-                                 seed=self.seed)
+        bfoa_customc = self.algo(population_size=10, n_chemotactic=100, n_reproduction=4, n_elimination=1,
+                                 step_size=0.1, seed=self.seed)
         AlgorithmTestCase.test_algorithm_run(self, bfoa_custom, bfoa_customc, max_evals=1000)
 
     def test_griewank_ngen(self):
         bfoa_custom = self.algo(population_size=10, n_chemotactic=100, n_reproduction=4, n_elimination=1, step_size=0.1,
                                 seed=self.seed)
-        bfoa_customc = self.algo(population_size=10, n_chemotactic=100, n_reproduction=4, n_elimination=1, step_size=0.1,
-                                 seed=self.seed)
+        bfoa_customc = self.algo(population_size=10, n_chemotactic=100, n_reproduction=4, n_elimination=1,
+                                 step_size=0.1, seed=self.seed)
         AlgorithmTestCase.test_algorithm_run(self, bfoa_custom, bfoa_customc, max_iters=1000)
 
 # vim: tabstop=3 noexpandtab shiftwidth=3 softtabstop=3
