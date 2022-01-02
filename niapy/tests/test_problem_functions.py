76d5ae06263c09c24843385751da899be3c0d365
zStupan <ziga.stupan@gmail.com>
2022-01-02 15:51:05 +0100

    using assert almost equal in alpine2 test

--- niapy/tests/test_problem_functions.py
+++ niapy/tests/test_problem_functions.py
@@ -134,7 +134,7 @@ class TestProblemFunctions(TestCase):
     def test_alpine2(self):
         """Test the alpine 2 function."""
         fun = get_problem('alpine2', self.dimension, 0.0, 10.0)
-        self.assertEqual(fun(self.array8), np.power(2.8081311800070053291, self.dimension))
+        self.assertAlmostEqual(fun(self.array8), np.power(2.8081311800070053291, self.dimension))
 
     def test_chung_reynolds(self):
         """Test the chung reynolds function."""
