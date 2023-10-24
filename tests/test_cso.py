ca7e2c8fac4e58e753620824a91629359ce45344
zStupan <ziga.stupan@gmail.com>
2023-10-24 15:39:26 +0200

    moved tests outside of package

---
+++ tests/test_cso.py
@@ -0,0 +1,20 @@
+# encoding=utf8
+
+from niapy.algorithms.basic import CatSwarmOptimization
+from tests.test_algorithm import AlgorithmTestCase, MyProblem
+
+
+class CSOTestCase(AlgorithmTestCase):
+    def setUp(self):
+        AlgorithmTestCase.setUp(self)
+        self.algo = CatSwarmOptimization
+
+    def test_custom(self):
+        cso_custom = self.algo(population_size=10, seed=self.seed)
+        cso_customc = self.algo(population_size=10, seed=self.seed)
+        AlgorithmTestCase.test_algorithm_run(self, cso_custom, cso_customc, MyProblem())
+
+    def test_griewank(self):
+        cso_griewank = self.algo(population_size=10, seed=self.seed)
+        cso_griewankc = self.algo(population_size=10, seed=self.seed)
+        AlgorithmTestCase.test_algorithm_run(self, cso_griewank, cso_griewankc)
